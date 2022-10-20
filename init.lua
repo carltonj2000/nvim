7db0eab8d0b57a91c5172b551ed3b44711db57bb
Carlton Joseph <carlton.joseph@gmail.com>
2022-10-19 19:56:50 -0700

    add lualine and bufferline

--- init.lua
+++ init.lua
@@ -1,5 +1,7 @@
 require('options');
 require('plugins');
 vim.cmd[[colorscheme tokyonight]]
+require('lualine-config');
+require('bufferline-config');
 require('treesitter-config');
 require('mappings');
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -4,6 +4,15 @@ return require('packer').startup(function(use)
   use 'lewis6991/impatient.nvim'
   use 'folke/tokyonight.nvim'
   use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
+  use {
+   'akinsho/bufferline.nvim',
+   tag = "v3.*",
+   requires = 'kyazdani42/nvim-web-devicons'
+  }
   use 'nvim-lua/plenary.nvim'
   use 'nvim-telescope/telescope.nvim'
   use 'folke/which-key.nvim'
