2e7e5cf6e18fcc6d5432f4bb0d0eb975df255ed6
Carlton Joseph <carlton.joseph@gmail.com>
2022-10-26 16:41:52 -0700

    update toggleterm setup

--- lua/toggleterm-config/init.lua
+++ lua/toggleterm-config/init.lua
@@ -6,4 +6,11 @@ require("toggleterm").setup({
 	start_in_insert = true,
 	persist_size = true,
 	direction = "horizontal",
+	size = 20 | function(term)
+		if term.direction == "horizontal" then
+			return 15
+		elseif term.direction == "vertical" then
+			return vim.o.columns * 0.4
+		end
+	end,
 })
