6b87371335190a524a0b343516222b44cedf7e91
Carlton Joseph <carlton.joseph@gmail.com>
2022-11-02 14:03:04 -0700

    configure lazyloading

--- init.lua
+++ init.lua
@@ -1,24 +1,3 @@
 require("options")
 require("keybindings")
-
 require("plugins")
-vim.cmd([[colorscheme tokyonight]])
-
-require("lualine-config")
-require("bufferline-config")
-
-require("nvim-tree-config")
-require("treesitter-config")
-require("autopairs-config")
-
-require("whichkey")
-
-require("telescope-config")
-
-require("lsp")
-
-require("colorizer-config")
-require("blankline-config")
-require("format-config")
-require("toggleterm-config")
-require("comment-config")
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -3,63 +3,127 @@ return require("packer").startup(function(use)
 
 	use("lewis6991/impatient.nvim")
 
-	use("folke/tokyonight.nvim")
+	use({
+		"folke/tokyonight.nvim",
+		config = "vim.cmd([[colorscheme tokyonight]])",
+	})
 
 	use({
 		"nvim-lualine/lualine.nvim",
 		requires = { "kyazdani42/nvim-web-devicons", opt = true },
+		event = "BufWinEnter",
+		config = "require('lualine-config')",
 	})
+
 	use({
 		"akinsho/bufferline.nvim",
 		tag = "v3.*",
 		requires = "kyazdani42/nvim-web-devicons",
+		event = "BufWinEnter",
+		config = "require('bufferline-config')",
 	})
 
 	use({
 		"nvim-tree/nvim-tree.lua",
 		requires = { "kyazdani42/nvim-web-devicons" },
+		cmd = "NvimTreeToggle",
+		config = "require('nvim-tree-config')",
 	})
 
-	use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
-	use("windwp/nvim-ts-autotag")
-	use("p00f/nvim-ts-rainbow")
-	use("windwp/nvim-autopairs")
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = ":TSUpdate",
+		event = "BufWinEnter",
+		config = "require('treesitter-config')",
+	})
 
-	use("nvim-treesitter/playground")
+	use({
+		"windwp/nvim-ts-autotag",
+		event = "InsertEnter",
+		after = "nvim-treesitter",
+	})
 
-	use("folke/which-key.nvim")
+	use({
+		"p00f/nvim-ts-rainbow",
+		after = "nvim-treesitter",
+	})
+
+	use({
+		"windwp/nvim-autopairs",
+		config = "require('autopairs-config')",
+		after = "nvim-cmp",
+	})
+
+	use({
+		"nvim-treesitter/playground",
+		after = "nvim-treesitter",
+	})
+
+	use({
+		"folke/which-key.nvim",
+		event = "BufWinEnter",
+		config = "require('whichkey-config')",
+	})
 
 	use({
 		"nvim-telescope/telescope.nvim",
 		requires = { { "nvim-lua/plenary.nvim" } },
+		cmd = "Telescope",
+		config = "require('telescope-config')",
 	})
 
-	use("neovim/nvim-lspconfig")
-	use("hrsh7th/cmp-nvim-lsp")
-	use("hrsh7th/cmp-buffer")
-	use("hrsh7th/cmp-path")
-	use("hrsh7th/cmp-cmdline")
-	use("hrsh7th/nvim-cmp")
-	use("hrsh7th/vim-vsnip")
-	use("onsails/lspkind.nvim")
+	use({
+    "neovim/nvim-lspconfig",
+    config = "require('lsp')",
+    after = "nvim-cmp",
+  })
+
+	use({ "hrsh7th/cmp-nvim-lsp", after = "nvim-cmp" })
+	use({ "hrsh7th/cmp-buffer", after = "nvim-cmp" })
+	use({ "hrsh7th/cmp-path", after = "nvim-cmp" })
+	use({ "hrsh7th/cmp-cmdline", after = "nvim-cmp" })
+	use({ "hrsh7th/nvim-cmp", event = "InsertEnter" })
+	use({ "hrsh7th/vim-vsnip", after = "nvim-cmp" })
+	use({ "onsails/lspkind.nvim", event = "BufRead" })
 	-- sinppet install
 	use("L3MON4D3/LuaSnip")
-	use("saadparwaiz1/cmp_luasnip")
+	use({"saadparwaiz1/cmp_luasnip", after = "nvim-cmp" })
 	use("rafamadriz/friendly-snippets")
 
-	use("norcalli/nvim-colorizer.lua")
+	use({
+		"norcalli/nvim-colorizer.lua",
+		config = "require('colorizer-config')",
+		event = "BufRead",
+	})
 
 	use({
 		"lewis6991/gitsigns.nvim",
+		requires = { { "nvim-lua/plenary.nvim" } },
 		config = function()
 			require("gitsigns").setup()
+			--require("gitsigns").setup({current_line_blame = true})
 		end,
 	})
 
-	use("lukas-reineke/indent-blankline.nvim")
+	use({
+    "lukas-reineke/indent-blankline.nvim",
+    config = "require('blankline-config')",
+    event = "BufRead"
+  })
 	--use "lukas-reineke/lsp-format.nvim"
-	use("mhartington/formatter.nvim")
+	use({
+    "mhartington/formatter.nvim",
+    config = "require('format-config')",
+    cmd = "Format"
+  })
 
-	use("akinsho/toggleterm.nvim")
-	use("terrortylor/nvim-comment")
+	use({
+    "akinsho/toggleterm.nvim",
+    config = "require('toggleterm-config')",
+  })
+	use({
+    "terrortylor/nvim-comment",
+    config = "require('comment-config')",
+    cmd = "CommentToggle"
+  })
 end)
